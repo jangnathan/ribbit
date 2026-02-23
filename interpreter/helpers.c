aef60de5ada747b592ef6e591f22733dc51e5968
Nathan Jang <nathanjang12@gmail.com>
2026-02-23 19:56:35 +0800

    create helper function file

---
+++ interpreter/helpers.c
@@ -0,0 +1,40 @@
+#include "helpers.h"
+
+uint8_t is_whitespace(char ch) {
+	if (ch == '\n' || ch == ' ' || ch == '\t') {
+		return 1;
+	}
+	return 0;
+}
+uint8_t is_lex(char ch) {
+	uint8_t chn = (uint8_t)ch;
+	if (chn >= 48 && chn <= 57) return 1;
+	if (chn >= 64 && chn <= 90) return 1;
+	if (chn >= 97 && chn <= 122) return 1;
+	if (ch == '_') return 1;
+	return 0;
+}
+uint8_t is_num(char ch) {
+	uint8_t chn = (uint8_t)ch;
+	if (chn >= 48 && chn <= 57) {
+		return 1;
+	}
+	return 0;
+}
+uint8_t char2digit(char ch) {
+	uint8_t chn = (uint8_t)ch;
+	return chn - 48;
+}
+uint8_t is_operator(char ch) {
+	if (ch=='+'||ch=='-'||ch=='/'||ch=='*'||
+		ch=='<'||ch=='>') {
+		return 1;
+	}
+	return 0;
+}
+
+uint8_t is_ender(char ch) {
+	if (ch == '+' || ch == ')' ||
+		is_lex(ch) || ch == '\0') return 1;
+	return 0;
+}
---
+++ interpreter/helpers.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <stdint.h>
+
+uint8_t is_whitespace(char ch);
+uint8_t is_lex(char ch);
+uint8_t is_num(char ch);
+uint8_t char2digit(char ch);
+uint8_t is_operator(char ch);
+uint8_t is_ender(char ch);
